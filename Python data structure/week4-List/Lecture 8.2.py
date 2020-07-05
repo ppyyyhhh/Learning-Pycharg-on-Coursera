363e2995c87c9198658253f4111877386344247b
ppyyyhhh <hpy0531@gmail.com>
2020-07-05 00:46:34 -0700

    07-05

---
+++ Python data structure/week4-List/Lecture 8.2.py
@@ -0,0 +1,42 @@
+# add list
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print('c=', c)
+print('a=', a)
+
+# slicing a list
+t = [1, 2, 3, 4, 5, 6, 7, 8, 9, ]
+print(t[1:3])
+print(t[:5])
+print(t[3:])
+print(t[:])
+
+# add things by append
+
+stuff = list()
+stuff.append('book')
+stuff.append(99)
+stuff.append('cookie')
+print(stuff)
+# sorting
+friends = ['Joseph', 'Glenn', 'Sally']
+friends.sort()
+print(friends)
+# enter number and output average
+
+print("enter numbers to get the mean, type done after finished")
+numlist = list()
+while True:
+    inp = input("Enter a number: ")
+    if inp == 'done' : break
+    try:
+        value = float(inp)
+    except:
+        print("invalid number")
+        continue
+    numlist.append(value)
+
+print(numlist)
+mean = sum(numlist) / len(numlist)
+print('The average is :', mean)
---
+++ Python data structure/week4-List/Lecture8.1 lists.py
@@ -0,0 +1,11 @@
+print(range(4))
+
+friends = ['Joseph','Glenn','Sally']
+print(len(friends))
+print(range(len(friends)))
+
+for friend in friends:
+    print('Happy New Year', friend)
+for i in range(len(friends)):
+    friend = friends[i]
+    print('Happy New Year:', friend)
